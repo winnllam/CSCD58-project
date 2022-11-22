ff898e3efe268d294b470ddd9acd1b3d32587bc8
Michelle Kee <michelle.hc.kee@gmail.com>
2022-11-22 00:14:14 -0500

    todos

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 # echo-server.py
 
 import socket
+import scapy
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -10,9 +11,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
+        # TODO: check for handshake first before receiving data
+        # Use packet.haslayer(TCP)
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
             conn.sendall(data)
+
+# TODO: Send SYN_ACK after receing SYN from client
+def send_syn_ack():
+    return
+
