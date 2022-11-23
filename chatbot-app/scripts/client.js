242213b88525ce62ed751aa7218406330b4a3a2f
Tony Cao <tony.cao1@outlook.com>
2022-11-22 22:34:51 -0500

    Added code for connection to server

---
+++ chatbot-app/scripts/client.js
@@ -0,0 +1,10 @@
+var connection = null;
+var host = "127.0.0.1";
+var port = "65432";
+
+function startConnection() {
+  var server = "ws://" + host + ":" + port;
+
+  connection = new WebSocket(server, "");
+  console.log("***CREATED CONNECTION");
+}
