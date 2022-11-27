9d3a85c87254394da4b3d66b3c839ddea1e48c4e
Tony Cao <tony.cao1@outlook.com>
2022-11-26 23:31:47 -0500

    Initial testing code for listening to message from server

--- chatbot-app/scripts/client.js
+++ chatbot-app/scripts/client.js
@@ -1,10 +1,15 @@
-var connection = null;
+var socket = null;
 var host = "127.0.0.1";
 var port = "65432";
 
 function startConnection() {
   var server = "ws://" + host + ":" + port + "/";
 
-  connection = new WebSocket(server);
+  socket = new WebSocket(server);
   console.log("***CREATED CONNECTION");
+
+  // Listen for messages
+  socket.addEventListener("message", (event) => {
+    console.log("Recieved message from server ", event.data);
+  });
 }
