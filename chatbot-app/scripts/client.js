05026f5e0aaff949e4ceecc1cac398f6b0454fc6
Tony Cao <tony.cao1@outlook.com>
2022-11-28 12:10:45 -0500

    Got message recieving from server

--- chatbot-app/scripts/client.js
+++ chatbot-app/scripts/client.js
@@ -1,17 +1,16 @@
 var dgram = require("dgram");
+const { Buffer } = require("node:buffer");
+
 var host = "127.0.0.1";
 var send_port = 65432;
 var listen_port = 65433;
-const { Buffer } = require("node:buffer");
 
 function startConnection() {
   var client = dgram.createSocket("udp4");
   client.bind(listen_port, host);
 
   const message = Buffer.from("Hello World");
-  client.send(message, send_port, host, (err) => {
-    client.close();
-  });
+  client.send(message, send_port, host, (err) => {});
 
   console.log("Info sent");
 
@@ -22,7 +21,13 @@ function startConnection() {
     );
   });
 
-  client.on("message", function (message, remote) {
-    console.log(remote.address + ":" + remote.port + " - " + message);
+  client.on("message", function (msg, info) {
+    console.log("Data received from server : " + msg);
+    console.log(
+      "Received %d bytes from %s:%d\n",
+      msg.length,
+      info.address,
+      info.port
+    );
   });
 }
