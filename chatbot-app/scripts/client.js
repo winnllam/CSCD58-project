8b1355705a7efc70beed795422e22e8b9e8ffa9b
Tony Cao <tony.cao1@outlook.com>
2022-11-28 11:50:01 -0500

    Got client listening to server

--- chatbot-app/scripts/client.js
+++ chatbot-app/scripts/client.js
@@ -1,15 +1,28 @@
 var dgram = require("dgram");
 var host = "127.0.0.1";
-var port = 65432;
+var send_port = 65432;
+var listen_port = 65433;
 const { Buffer } = require("node:buffer");
-
-// Documentation: https://nodejs.org/api/dgram.html
 
 function startConnection() {
   var client = dgram.createSocket("udp4");
+  client.bind(listen_port, host);
 
   const message = Buffer.from("Hello World");
-  client.send(message, port, host, (err) => {
+  client.send(message, send_port, host, (err) => {
     client.close();
+  });
+
+  console.log("Info sent");
+
+  client.on("listening", function () {
+    var address = client.address();
+    console.log(
+      "UDP Server listening on " + address.address + ":" + address.port
+    );
+  });
+
+  client.on("message", function (message, remote) {
+    console.log(remote.address + ":" + remote.port + " - " + message);
   });
 }
