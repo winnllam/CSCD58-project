c3c9e457eac63395ed8d7791d38bc5ed49ec8a8e
Tony Cao <tony.cao1@outlook.com>
2022-11-27 19:21:00 -0500

    Got basic message sending

--- chatbot-app/scripts/client.js
+++ chatbot-app/scripts/client.js
@@ -1,11 +1,13 @@
 var dgram = require("dgram");
 var host = "127.0.0.1";
 var port = 65432;
+const { Buffer } = require("node:buffer");
 
 function startConnection() {
   var client = dgram.createSocket("udp4");
-  console.log(port);
 
-  client.connect(port, host);
-  console.log("***CREATED CONNECTION");
+  const message = Buffer.from("Hello World");
+  client.send(message, port, host, (err) => {
+    client.close();
+  });
 }
