cee9ffdb0bc6a83798d216efdb68da4c9b150aa7
winnllam <winnieee.lam00@gmail.com>
2022-11-28 23:29:04 -0500

    Client sending packet to server

--- chatbot-app/scripts/client.js
+++ chatbot-app/scripts/client.js
@@ -6,12 +6,17 @@ var host = "127.0.0.1";
 var send_port = 65432;
 var listen_port = 65433;
 
-function startConnection() {
+window.startConnection = function() {
   var client = dgram.createSocket("udp4");
   client.bind(listen_port, host);
 
   const message = Buffer.from("Hello World");
   client.send(message, send_port, host, (err) => {});
+
+  var pkt = new TCPPacket(1234, send_port, 1, 1);
+  console.log("Client is sending the following packet:");
+  console.log(pkt.encode());
+  client.send(pkt.encode(), send_port, host, (err) => {});
 
   console.log("Info sent");
 
@@ -36,4 +41,26 @@ function startConnection() {
     var data = struct.unpack("!HHIIBHHHHII", Buffer.from(msg, "hex"));
     console.log("Decoded struct is " + data);
   });
+}
+
+
+class TCPPacket {
+  constructor(src_port, dst_port, seq_num, ack_num, flags=0, checksum=0, options=0, data=0) {
+      this.src_port = src_port;
+      this.dst_port = dst_port;
+      this.seq_num = seq_num;
+      this.ack_num = ack_num;
+      this.flags = flags;
+      this.checksum = checksum;
+      this.options = options;
+      this.data = data;
+  }
+
+  encode() {
+    return struct.pack("!HHIIBHHHHII", this.src_port, this.dst_port, this.seq_num, this.ack_num, 0, this.flags, 0, 0, 0, this.options, this.data);
+  }
+
+  decode() {
+    
+  }
 }
