65142c340b5d683f2f24650bf2a977c8e4bc84ff
Tony Cao <tony.cao1@outlook.com>
2022-11-29 11:44:49 -0500

    Added decode func

--- chatbot-app/scripts/client.js
+++ chatbot-app/scripts/client.js
@@ -1,17 +1,18 @@
 var dgram = require("dgram");
 const { Buffer } = require("node:buffer");
 const struct = require("python-struct");
+const packetType = "!HHIIBHHHHII";
 
 var host = "127.0.0.1";
 var send_port = 65432;
 var listen_port = 65433;
 
-window.startConnection = function() {
+window.startConnection = function () {
   var client = dgram.createSocket("udp4");
   client.bind(listen_port, host);
 
-  const message = Buffer.from("Hello World");
-  client.send(message, send_port, host, (err) => {});
+  // const message = Buffer.from("Hello World");
+  // client.send(message, send_port, host, (err) => {});
 
   var pkt = new TCPPacket(1234, send_port, 1, 1);
   console.log("Client is sending the following packet:");
@@ -20,6 +21,7 @@ window.startConnection = function() {
 
   console.log("Info sent");
 
+  // TODO: Figure out if this is even needed tbh
   client.on("listening", function () {
     var address = client.address();
     console.log(
@@ -37,30 +39,52 @@ window.startConnection = function() {
     );
 
     // TODO: Decode the data
-    struct.sizeOf("!HHIIBHHHHII");
-    var data = struct.unpack("!HHIIBHHHHII", Buffer.from(msg, "hex"));
-    console.log("Decoded struct is " + data);
+    // struct.sizeOf(packetType);
+    // var data = struct.unpack(packetType, Buffer.from(msg, "hex"));
+    // console.log("Decoded struct is " + data);
+    var data = pkt.decode(msg);
   });
-}
-
+};
 
 class TCPPacket {
-  constructor(src_port, dst_port, seq_num, ack_num, flags=0, checksum=0, options=0, data=0) {
-      this.src_port = src_port;
-      this.dst_port = dst_port;
-      this.seq_num = seq_num;
-      this.ack_num = ack_num;
-      this.flags = flags;
-      this.checksum = checksum;
-      this.options = options;
-      this.data = data;
+  constructor(
+    src_port,
+    dst_port,
+    seq_num,
+    ack_num,
+    flags = 0,
+    checksum = 0,
+    options = 0,
+    data = 0
+  ) {
+    this.src_port = src_port;
+    this.dst_port = dst_port;
+    this.seq_num = seq_num;
+    this.ack_num = ack_num;
+    this.flags = flags;
+    this.checksum = checksum;
+    this.options = options;
+    this.data = data;
   }
 
   encode() {
-    return struct.pack("!HHIIBHHHHII", this.src_port, this.dst_port, this.seq_num, this.ack_num, 0, this.flags, 0, 0, 0, this.options, this.data);
+    return struct.pack(
+      "!HHIIBHHHHII",
+      this.src_port,
+      this.dst_port,
+      this.seq_num,
+      this.ack_num,
+      0,
+      this.flags,
+      0,
+      0,
+      0,
+      this.options,
+      this.data
+    );
   }
 
-  decode() {
-    
+  decode(data) {
+    return struct.unpack(packetType, Buffer.from(data, "hex"));
   }
 }
