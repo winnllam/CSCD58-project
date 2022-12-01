8fa9556720d3be92f92f3cc3810064266ef44e4c
Michelle Kee <michelle.kee@mail.utoronto.ca>
2022-12-01 15:25:31 -0500

    clean

--- server.py
+++ server.py
@@ -2,9 +2,6 @@ import socket
 from packet.packet import TCPPacket
 import struct
 
-# CLIENT_HOST = "127.0.0.1"
-# CLIENT_PORT = 65433
-
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
@@ -21,42 +18,12 @@ def decode_packet_flag_byte(data):
     flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
     return flag_byte
 
-# After receiving ack it will start connection
-def receive_ack(s):
-    data, addr = s.recvfrom(1024)
-    if(decode_packet_flag_byte(data) == "00010000"):
-        print("Yes. ACK packet received.")
-        return addr
-    return ()
-
 def send_syn_ack(s, client_host, client_port):
     pkt = TCPPacket(src_port=PORT, dst_port=client_port, seq_num=2, ack_num=1, syn=1, ack=1)
     print("SYN-ACK: The server is sending the following packet:")
     print(pkt.encode())
     s.sendto(pkt.encode(), (client_host, client_port))
     return
-
-def receive_syn(s):
-    data, addr = s.recvfrom(1024)
-    print("Server received the following packet")
-    print(data)
-    if(decode_packet_flag_byte(data) == "00000010"):
-        print("Yes. SYN packet received.")
-        return addr
-    return ()
-
-def receive_tcp_connection(s):
-    addr = receive_syn(s)
-    if not addr:
-        print("Not a syn packet. Wait for another syn packet, restart TCP process")
-        return
-    send_syn_ack(s, addr[0], addr[1])
-    addr = receive_ack(s)
-    print("TCP Handshake complete. Established connection wtih")
-    print(addr)
-    # Return address, add to active connections
-    return addr
-    
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
