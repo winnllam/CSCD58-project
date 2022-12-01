bfb4636761ef6ffd74e6b9a5cae7a6bc3984564d
Michelle Kee <michelle.hc.kee@gmail.com>
2022-11-30 23:19:08 -0500

    full tcp handshake serverside

--- client.py
+++ client.py
@@ -10,7 +10,8 @@ PORT = 65432  # The port used by the server
 
 # TODO: Send SYN to server to initiate TCP connection with the server
 def send_syn():
-
+    pkt = TCPPacket(src_port = 1234, dst_port = PORT, seq_num = 1, ack_num = 1, syn=1)
+    s.sendto(pkt.encode(), (HOST, PORT))
     # pkt = TCPPacket(src_port = 1234, dst_port = PORT, seq_num = 1, ack_num = 1)
     # print(pkt.encode())
     # print (pkt.encode().decode())
@@ -18,7 +19,8 @@ def send_syn():
 
 # TODO: Send ACK to server after receiving SYN_ACK from server
 def send_ack():
-    pkt = TCPPacket(src_port = 1234, dst_port = PORT, seq_num = 1, ack_num = 1)
+    pkt = TCPPacket(src_port = 1234, dst_port = PORT, seq_num = 1, ack_num = 1, ack=1)
+    s.sendto(pkt.encode(), (HOST, PORT))
     # print(pkt.encode())
     # print (pkt.encode().decode())
     return
@@ -27,7 +29,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((CLIENT_HOST, CLIENT_PORT))
     send_syn()
     # This is sending a message directly to host/port without TCP connection.
-    s.sendto(b"Hello World", (HOST, PORT))
+    # s.sendto(b"Hello World", (HOST, PORT))
     data = s.recv(1024)
+    send_ack()
     print(data)
 
--- server.py
+++ server.py
@@ -8,8 +8,26 @@ import struct
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-# TODO: Send SYN_ACK after receing SYN from client
+# Helper function to decode packet from byte to TCPPacket structure
+def decode_packet(data):
+    decoded_data = struct.unpack("!HHIIBBHHHII", data)
+    flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
+    received_packet = TCPPacket(src_port=decoded_data[0], dst_port=decoded_data[1], seq_num=int(decoded_data[2])+1, ack_num=decoded_data[3], cwr=flag_byte[0], ece=flag_byte[1], urg=flag_byte[2], ack=flag_byte[3], psh=flag_byte[4], rst=flag_byte[5], syn=flag_byte[6], fin=flag_byte[7], checksum=decoded_data[6], options=decoded_data[7], data=decoded_data[8])
+    return received_packet
 
+# Helper fuynction to decode packet from byte form, and return the flag byte string
+def decode_packet_flag_byte(data):
+    decoded_data = struct.unpack("!HHIIBBHHHII", data)
+    flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
+    return flag_byte
+
+# After receiving ack it will start connection
+def receive_ack(s):
+    data, addr = s.recvfrom(1024)
+    if(decode_packet_flag_byte(data) == "00010000"):
+        print("Yes. ACK packet received.")
+        return addr
+    return ()
 
 def send_syn_ack(s, client_host, client_port):
     pkt = TCPPacket(src_port=PORT, dst_port=client_port, seq_num=2, ack_num=1, syn=1, ack=1)
@@ -22,36 +40,27 @@ def receive_syn(s):
     data, addr = s.recvfrom(1024)
     print("Server received the following packet")
     print(data)
-
-    decoded_data = struct.unpack("!HHIIBBHHHII", data)
-    print("Server is decoding:")
-    print(decoded_data)
-    flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
-    print("Decoded flag:")
-    print(flag_byte)
-    # Rebuilding packet (Not necessary in this case but needed in future)
-    # Ignore decoded_data[4] -> data offset + NS
-    received_packet = TCPPacket(src_port=decoded_data[0], dst_port=decoded_data[1], seq_num=int(decoded_data[2])+1, ack_num=decoded_data[3], cwr=flag_byte[0], ece=flag_byte[1], urg=flag_byte[2], ack=flag_byte[3], psh=flag_byte[4], rst=flag_byte[5], syn=flag_byte[6], fin=flag_byte[7], checksum=decoded_data[6], options=decoded_data[7], data=decoded_data[8])
-    # Technically don't need to seperate each flag_byte and r and can check for all flags like this:
-    if(flag_byte == "00000010"):
+    if(decode_packet_flag_byte(data) == "00000010"):
         print("Yes. SYN packet received.")
         return addr
-    # Otherwise this is not syn packet, return empty
     return ()
 
 def receive_tcp_connection(s):
     addr = receive_syn(s)
     if not addr:
-        print("Not a syn packet. Wait for another syn packet.")
+        print("Not a syn packet. Wait for another syn packet, restart TCP process")
         return
     send_syn_ack(s, addr[0], addr[1])
+    addr = receive_ack(s)
+    print("TCP Handshake complete. Established connection wtih")
+    print(addr)
+    # Return address, add to active connections
+    return addr
+    
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
+    connections = []
     while True:
-        receive_tcp_connection(s)
-        # TODO: GET THE HOST/PORT OF CLIENT ON FIRST COMMUNICATION.
-        # ASSUMING WE KNOW THEIR HOST AND PORT AT THIS POINT
-        # FOR TESTING. CONSTANTLY SENDINGG TCP PACKET.
-        # Listen on address
+        connections.append(receive_tcp_connection(s))
         data, addr = s.recvfrom(1024)
