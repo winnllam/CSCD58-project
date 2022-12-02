c5ea20a3ba60ae33a435fa93ad0b9459ccc36ccf
Michelle Kee <michelle.hc.kee@gmail.com>
2022-12-02 17:10:02 -0500

    seq num fix

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 def decode_packet(data):
     decoded_data = struct.unpack("!HHIIBBHHHII", data)
     flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
-    received_packet = TCPPacket(src_port=decoded_data[0], dst_port=decoded_data[1], seq_num=int(decoded_data[2])+1, ack_num=decoded_data[3], cwr=flag_byte[0], ece=flag_byte[1], urg=flag_byte[2], ack=flag_byte[3], psh=flag_byte[4], rst=flag_byte[5], syn=flag_byte[6], fin=flag_byte[7], checksum=decoded_data[6], options=decoded_data[7], data=decoded_data[8])
+    received_packet = TCPPacket(src_port=decoded_data[0], dst_port=decoded_data[1], seq_num=int(decoded_data[2]), ack_num=decoded_data[3], cwr=flag_byte[0], ece=flag_byte[1], urg=flag_byte[2], ack=flag_byte[3], psh=flag_byte[4], rst=flag_byte[5], syn=flag_byte[6], fin=flag_byte[7], checksum=decoded_data[6], options=decoded_data[7], data=decoded_data[8])
     return received_packet
 
 # Helper fuynction to decode packet from byte form, and return the flag byte string
@@ -18,8 +18,8 @@ def decode_packet_flag_byte(data):
     flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
     return flag_byte
 
-def send_syn_ack(s, client_host, client_port):
-    pkt = TCPPacket(src_port=PORT, dst_port=client_port, seq_num=2, ack_num=1, syn=1, ack=1)
+def send_syn_ack(s, client_host, client_port, seq_num):
+    pkt = TCPPacket(src_port=PORT, dst_port=client_port, seq_num=seq_num, ack_num=1, syn=1, ack=1)
     print("SYN-ACK: The server is sending the following packet:")
     print(pkt.encode())
     s.sendto(pkt.encode(), (client_host, client_port))
@@ -48,7 +48,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         # Case 1: SYN request
         if (pkt_flag == "00000010"):
             # Syn request received, so send SYN-ACK
-            send_syn_ack(s, addr[0], addr[1])
+            send_syn_ack(s, addr[0], addr[1], pkt.seq_num+1)
             # Wait for ACK response
             ack_waiting.append(addr)
         # Case 2: ACK request 
