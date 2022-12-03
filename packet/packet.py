eb6bf5af8b2d71b3c3c82be0e83a08a249ce1b92
Michelle Kee <michelle.hc.kee@gmail.com>
2022-12-02 23:24:20 -0500

    wip data

--- packet/packet.py
+++ packet/packet.py
@@ -1,5 +1,6 @@
 import struct
-PACKET_TYPE = "!HHIIBBHHHI300p"
+PACKET_TYPE = "!HHIIBBHHHI"
+DATA_LEN = 300
 
 
 class TCPPacket:
@@ -47,4 +48,5 @@ class TCPPacket:
         # Converts the flags directly to a byte string. 0 is no flag, 1 represents flag active
         flags = str(self.cwr) + str(self.ece) + str(self.urg) + str(self.ack) + \
             str(self.psh) + str(self.rst) + str(self.syn) + str(self.fin)
-        return struct.pack(PACKET_TYPE, self.src_port, self.dst_port, self.seq_num, self.ack_num, 0, int(flags.encode(), base=2), 0, 0, 0, self.options, self.data)
+        encoded_data = str.encode(self.data.rjust(300, ' '))
+        return struct.pack(PACKET_TYPE, self.src_port, self.dst_port, self.seq_num, self.ack_num, 0, int(flags.encode(), base=2), 0, 0, 0, self.options) + encoded_data
--- server.py
+++ server.py
@@ -4,23 +4,23 @@ import struct
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-PACKET_TYPE = "!HHIIBBHHHI300p"
+PACKET_TYPE = "!HHIIBBHHHI"
 
 # Helper function to decode packet from byte to TCPPacket structure
 
 
 def decode_packet(data):
-    decoded_data = struct.unpack(PACKET_TYPE, data)
+    decoded_data = struct.unpack(PACKET_TYPE, data[:-300])
     flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
     received_packet = TCPPacket(src_port=decoded_data[0], dst_port=decoded_data[1], seq_num=int(decoded_data[2]), ack_num=decoded_data[3], cwr=flag_byte[0], ece=flag_byte[1], urg=flag_byte[2],
-                                ack=flag_byte[3], psh=flag_byte[4], rst=flag_byte[5], syn=flag_byte[6], fin=flag_byte[7], checksum=decoded_data[6], options=decoded_data[7], data=decoded_data[8])
+                                ack=flag_byte[3], psh=flag_byte[4], rst=flag_byte[5], syn=flag_byte[6], fin=flag_byte[7], checksum=decoded_data[6], options=decoded_data[7], data=data[-300:])
     return received_packet
 
 # Helper fuynction to decode packet from byte form, and return the flag byte string
 
 
 def decode_packet_flag_byte(data):
-    decoded_data = struct.unpack(PACKET_TYPE, data)
+    decoded_data = struct.unpack(PACKET_TYPE, data[:-300])
     flag_byte = bin(decoded_data[5])[2:].rjust(8, '0')
     return flag_byte
 
@@ -83,6 +83,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             send_syn_ack(s, addr[0], addr[1], pkt.seq_num+1)
             # Wait for ACK response
             ack_waiting.append(addr)
+            print("packet data")
+            print(pkt.data)
         # Case 2: ACK request
         elif (pkt_flag == "00010000" and addr in ack_waiting):
             # Establish connection, ready to receive
