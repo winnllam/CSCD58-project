79e03112cf60fe66629cefc33da3a1bd26402089
Michelle Kee <michelle.hc.kee@gmail.com>
2022-12-02 19:35:19 -0500

    tcp fin

--- server.py
+++ server.py
@@ -25,20 +25,39 @@ def send_syn_ack(s, client_host, client_port, seq_num):
     s.sendto(pkt.encode(), (client_host, client_port))
     return
 
+def send_fin_ack(s, client_host, client_port, seq_num):
+    pkt = TCPPacket(src_port=PORT, dst_port=client_port, seq_num=seq_num, ack_num=1, fin=1, ack=1)
+    print("FIN-ACK: The server is sending the following packet:")
+    print(pkt.encode())
+    s.sendto(pkt.encode(), (client_host, client_port))
+    return
+
+def send_fin(s, client_host, client_port, seq_num):
+    pkt = TCPPacket(src_port=PORT, dst_port=client_port, seq_num=seq_num, ack_num=1, fin=1)
+    print("FIN: The server is sending the following packet:")
+    print(pkt.encode())
+    s.sendto(pkt.encode(), (client_host, client_port))
+    return
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
     # List of addresses (host, port) where the connection is established
     connections = []
     # List of addresses (host, port) where we are awaiting to receive ACK packet to confirm connection
     ack_waiting = []
+    # List of address on passive close
+    passive_close = []
     # TODO: low-prio, timeout time for ack_waiting.
     ack_timeouts = []
     while True:
         # When a packet is received it can be...
-        # A: TCP connection
+        # A: TCP handshake - start connection
             # receive SYN - Send SYN-ACK
             # receive ACK - Connection established
-        # B: Data from established connection
+        # C: Data from established connection
+        # B: TCP end connection (https://www.geeksforgeeks.org/tcp-connection-termination/)
+            # Receive FIN
+            # Receive ACK of FIN
         
         # Receive packet data and process it
         data, addr = s.recvfrom(1024)
@@ -57,7 +76,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             ack_waiting.remove(addr)
             connections.append(addr)
             print(connections)
-        # Case 3: Connection is already established
+        # Case 3: Received FIN request
+        elif (pkt_flag == "00000001" and addr in connections):
+            # Send ACK to client for them to enter FIN_WAIT_2
+            send_fin_ack(s, addr[0], addr[1], pkt.seq_num+1)
+            # Send FIN for client to enter TIME_WAIT
+            send_fin(s, addr[0], addr[1], pkt.seq_num+1)
+            # Passive close connection
+            passive_close.append(addr)
+        # Case 4: Received FIN-AKCK
+        elif (pkt_flag == "00010001" and addr in passive_close):
+            # Officially closeconnection
+            passive_close.remove(addr)
+            connections.remove(addr)
+        # Case 5: Connection is already established
         elif (addr in connections):
             print("todo")
             # TODO: do something with the data
